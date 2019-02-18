7a42b5caba51ce0ff1e93c8bfee449c99aae1a6a
dinghu <dinghu@fkhwl.com>
2019-02-18 11:21:35 +0800

    sql filter

--- my-shop-web/src/main/java/com/fengdu/dao/AdPojoMapper.java
+++ my-shop-web/src/main/java/com/fengdu/dao/AdPojoMapper.java
@@ -1,10 +1,14 @@
 package com.fengdu.dao;
 
+import com.fengdu.mysql.FilterRule;
+import com.fengdu.mysql.PageQuery;
 import com.fengdu.pojo.AdPojo;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Map;
 
 public interface AdPojoMapper {
     List<AdPojo> queryList(Map<String, Object> map);
+    List<AdPojo> findByPage(@Param("filterRules") List<FilterRule> filterRules, @Param("pageQuery") PageQuery pageQuery);
 }
---
+++ my-shop-web/src/main/java/com/fengdu/mysql/FilterRule.java
@@ -0,0 +1,42 @@
+package com.fengdu.mysql;
+
+
+public class FilterRule {
+	private String key;
+	private String comparator;
+	private Object value;
+
+	public FilterRule() {
+	}
+
+	public FilterRule(String key, String comparator, Object value) {
+		this.key = key;
+		this.comparator = comparator;
+		this.value = value;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public String getComparator() {
+		return comparator;
+	}
+
+	public void setComparator(String comparator) {
+		this.comparator = comparator;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+
+}
