205ed6fe62dc1907979b8324c9578b37674d9fa7
dinghu <dinghu@fkhwl.com>
2020-03-04 14:00:57 +0800

    历史价格查询修改

--- my-shop-manager/src/main/java/com/fengdu/api/ConfigerController.java
+++ my-shop-manager/src/main/java/com/fengdu/api/ConfigerController.java
@@ -102,7 +102,17 @@ public class ConfigerController extends BaseController {
             }
         }
 
+
         List<GoodsEntity> recommendCommodities = goodsService.queryList(paramRecommend);
+        if (recommendCommodities == null || recommendCommodities.isEmpty()) {
+            if (!TextUtils.isEmpty(keywords)) {
+                Map paramRecommend1 = new HashMap();
+                paramRecommend1.put("isDelete", 0);
+                paramRecommend1.put("offset", 0);
+                paramRecommend1.put("limit", 40);
+                recommendCommodities = goodsService.queryList(paramRecommend1);
+            }
+        }
 
         Map paramConfig = new HashMap();
         paramConfig.put("isVisible", 0);
